22e0dbcc4794a25db087f96a7faeacc1eaae72c0
HECTOR MAURICIO RODRIGUEZ SALAZAR <mauhector7@gmail.com>
2024-04-10 20:58:09 -0600

    Hasta aqui funciona

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CamaraXApp08-04-2024"
+include(":app")
+ 
